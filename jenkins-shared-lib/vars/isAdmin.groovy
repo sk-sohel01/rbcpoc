08b9a485fcd98994a24a92362446c2e050a2fb20
sk-sohel01 <sohel.sk.01.12.94@gmail.com>
2025-05-02 16:53:38 +0530

    Update isAdmin.groovy

--- jenkins-shared-lib/vars/isAdmin.groovy
+++ jenkins-shared-lib/vars/isAdmin.groovy
@@ -1,13 +1,21 @@
-def userId = currentBuild.rawBuild.getCauses().find { it.userId }?.userId?.trim()
-if (!userId) return false
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy
+import jenkins.model.Jenkins
 
-def authStrategy = Jenkins.get().getAuthorizationStrategy()
-if (!(authStrategy instanceof RoleBasedAuthorizationStrategy)) return false
+def call() {
+    def userId = currentBuild.rawBuild.getCauses().find { it.userId }?.userId?.trim()
+    println "🧾 Detected userId: '${userId}'"
 
-def roleMap = authStrategy.getRoleMap(RoleBasedAuthorizationStrategy.GLOBAL)
-def adminRole = roleMap.getRole('admin')
+    if (!userId) return false
 
-if (!adminRole) return false
+    def authStrategy = Jenkins.get().getAuthorizationStrategy()
+    if (!(authStrategy instanceof RoleBasedAuthorizationStrategy)) return false
 
-def assignedSids = roleMap.getSids(adminRole)*.toLowerCase()
-return assignedSids.contains(userId.toLowerCase())
+    def roleMap = authStrategy.getRoleMap(RoleBasedAuthorizationStrategy.GLOBAL)
+    def adminRole = roleMap.getRole('admin')
+    if (!adminRole) return false
+
+    def assignedSids = roleMap.getSids(adminRole)*.toLowerCase()
+    println "🔐 Admin role members: ${assignedSids}"
+
+    return assignedSids.contains(userId.toLowerCase())
+}
