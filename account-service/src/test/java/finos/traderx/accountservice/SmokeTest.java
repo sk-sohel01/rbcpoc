9737a69e1a1fad325ffedd84fb2ecc42953c3a1d
sk-sohel01 <sohel.sk.01.12.94@gmail.com>
2025-05-13 19:00:06 +0530

    gradle test

---
+++ account-service/src/test/java/finos/traderx/accountservice/SmokeTest.java
@@ -0,0 +1,12 @@
+package finos.traderx.accountservice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SmokeTest {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
+
